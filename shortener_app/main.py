26ea428a9534670005f9c2bce3871e7f29d0b35d
DiasErick <116904851+DiasErick@users.noreply.github.com>
2022-11-14 13:15:06 -0400

    Connecting with database

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,19 +1,54 @@
-from fastapi import FastAPI, HTTPException
-from . import schemas
-import validators
+import secrets
 
+import validators
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from . import models, schemas
+from .database import SessionLocal, engine
+
+app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+def read_root():    
     return "Welcome to my first experience with API in Python!"
+
+@app.get("/all")
+def reat_all():
+    from shortener_app.database import SessionLocal
+    from shortener_app.models import URL
+    db = SessionLocal()
+    return db.query(URL).all()
+    
 
 def raise_bad_request(message):
     raise HTTPException(status_code=400, detail=message)
 
-@app.post("/url")
-def create_url(url: schemas.URLBase):
+@app.post("/url", response_model=schemas.URLInfo)
+def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     if not validators.url(url.target_url):
-        raise raise_bad_request("It seems that your provided URL it's not valid. Let's try again?")
-    return f"TODO: Create database entry for: {url.target_url}"
+        raise_bad_request(message="Your provided URL is not valid")
+
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    key = "".join(secrets.choice(chars) for _ in range(5))
+    secret_key = "".join(secrets.choice(chars) for _ in range(8))
+    db_url = models.URL(
+        target_url=url.target_url, key=key, secret_key=secret_key
+    )
+    db.add(db_url)
+    db.commit()
+    db.refresh(db_url)
+    db_url.url = key
+    db_url.admin_url = secret_key
+
+    return db_url
